030ea1f59f57a11d3e6d827c1ae9b92da917b129
acidrums7 <edoardo.carradori.96@gmail.com>
2023-11-11 15:11:49 +0100

    Basic Solidity: Interacting with other Contracts

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,7 +1,7 @@
 // I'm a comment!
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.23;
 
 // pragma solidity ^0.8.0;
 // pragma solidity >=0.8.0 <0.9.0;
@@ -31,3 +31,6 @@ contract SimpleStorage {
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
+
+contract SimpleStorage2 {}
+contract SimpleStorage3 {}
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,9 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.23;
+
+// named import
+import {SimpleStorage /*, SimpleStorage3*/ } from "./SimpleStorage.sol";
 
 contract StorageFactory {
+    SimpleStorage[] public simpleStorageContractsList;
+
     function createSimpleStorageContract() public {
         // How does the storage factory knows what SimpleStorage looks like?
+        // 'new' keyword is used to create contracts, not struct
+        simpleStorageContractsList.push(new SimpleStorage());
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newFavoriteNumber)
+        public
+    {
+        // Contract Address
+        // ABI -> Application Binary Interface
+        simpleStorageContractsList[_simpleStorageIndex].store(
+            _newFavoriteNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageContractsList[_simpleStorageIndex].retrieve();
     }
 }

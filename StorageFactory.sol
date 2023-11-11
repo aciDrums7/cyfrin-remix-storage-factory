a444d71337dd53b17e5f977138788a2ca14fd308
acidrums7 <edoardo.carradori.96@gmail.com>
2023-11-11 12:45:23 +0100

    WIP: Created StorageFactory contract

---
+++ StorageFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract StorageFactory {
+    function createSimpleStorageContract() public {
+        // How does the storage factory knows what SimpleStorage looks like?
+    }
+}
